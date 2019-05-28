a1017c8837f46871167eba1a4869bdc17302a496
Jessica <jessrkaspar@gmail.com>
2019-05-28 12:03:45 -0500

    Checkpoint 11 Bloc Jams: Complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -25,4 +25,12 @@
      const nextSong = album.songs[nextSongIndex];
      player.playPause(nextSong);
    });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control input').val(percent);
+  }, 1000);
 }
