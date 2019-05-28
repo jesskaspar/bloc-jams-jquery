6ff5ca45aab1e55d40435b4412fddfe9da3bd25e
Jessica <jessrkaspar@gmail.com>
2019-05-28 14:48:40 -0500

    Checkpoint 10 Revision

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,14 +16,12 @@
    });
 
    $('button#previous').on('click', function() {
-     if (player.playState !== 'playing') { return; }
-
-     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-     const nextSongIndex = currentSongIndex - 1;
-     if (nextSongIndex >= album.songs.length) { return; }
-
-     const nextSong = album.songs[nextSongIndex];
-     player.playPause(nextSong);
+       if (player.playState !== 'playing') { return; }
+       const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+       const prevSongIndex = currentSongIndex - 1;
+       if (prevSongIndex < 0) { return; }
+       const prevSong = album.songs[prevSongIndex];
+       player.playPause(prevSong);
    });
 
   setInterval( () => {
